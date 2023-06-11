2080e718a748fdc85af7efb4f8913713ffa650e1
jose <jose@LAPTOP-F7CMF4TI.localdomain>
2023-06-11 14:04:57 -0400

    Final Version

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,29 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let dragonNames = dragons.map(d => d.name);
+    return dragonNames;
+  }
+}
+
+module.exports = Dragon;
+//Tests
+// const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+// const toothless = new Dragon("Toothless", "black");
+// console.log(toothless);
+// console.log(toothless.breathesFire());
+// console.log(Dragon.getDragons(puff, toothless));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,38 @@
 // Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis){
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner(){
+        let name = this.name;
+        this.evilDoings.forEach(function(ed){
+            console.log(`${name} will ${ed}`);
+        })
+    }
+
+    burnsNemesis(){
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+module.exports = EvilDragon;
+
+//Test
+// const enderDragon = new EvilDragon(
+//     "Ender Dragon",
+//     "black", [
+//       "spit a fireball at Steve",
+//       "rule over all the Endermen",
+//       "destroy all blocks in its way"
+//     ],
+//     "Steve"
+//   );
+//   console.log(enderDragon);
+//   enderDragon.dontInviteThemOverForDinner();
+//   console.log(enderDragon.breathesFire());
+//   console.log(enderDragon.burnsNemesis());
